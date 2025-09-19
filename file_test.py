8904b8adfb2f23157e1f9cc274168401574179c5
Charles Lenk <lenkc@Charless-MacBook-Air.local>
2025-09-18 18:49:07 -0700

    File picker test

--- file_test.py
+++ file_test.py
@@ -7,10 +7,15 @@ from functools import cached_property
 from threading import Lock
 from enum import StrEnum, auto
 from pathlib import Path
-from tkinter import filedialog
+from tkinter import filedialog, Tk
 import ctypes
 
-ctypes.windll.user32.SetProcessDPIAware()
 
-test = filedialog.askdirectory()
+# ctypes.windll.user32.SetProcessDPIAware()
+root = Tk()
+root.wm_attributes('-alpha', 0)
+root.wm_attributes('-topmost', 1)
+test = filedialog.askdirectory(parent=root)
+test = filedialog.askopenfilename(parent=root)
 print(test)
+
