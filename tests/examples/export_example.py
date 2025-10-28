bff2f02fb28177894598fa5332621d35b8793b78
Charles Lenk <lenk.charles@gmail.com>
2025-10-27 19:08:46 -0700

    Add user args to file name

--- tests/examples/export_example.py
+++ tests/examples/export_example.py
@@ -11,25 +11,25 @@ exportables=Folder(
             contents=[
                 # Override file_name to export each cube to a separate file, rather than overwriting the same file.
                 # x, y, and z are user-defined arguments that are passed to the export .scad file.
-                Model(name='cube', file_name='cube_5', x=5, y=5, z=5),
-                Model(name='cube', file_name='cube_10', x=10, y=10, z=10),
-                Model(name='cube', file_name='cube_15', x=15, y=15, z=15)
+                Model(name='cube', x=5, y=5, z=5),
+                Model(name='cube', x=10, y=10, z=10),
+                Model(name='cube', x=15, y=15, z=15)
             ]
         ),
         Folder(
             name='cylinders',
             contents=[
-                Model(name='cylinder', file_name='cylinder_10', d=10, z=10),
-                Model(name='cylinder', file_name='cylinder_20', d=10, z=20),
-                Model(name='cylinder', file_name='cylinder_30', d=10, z=30)
+                Model(name='cylinder', d=10, z=10),
+                Model(name='cylinder', d=10, z=20),
+                Model(name='cylinder', d=10, z=30)
             ]
         ),
         Folder(
             name='spheres',
             contents=[
-                Model(name='sphere', file_name='sphere_15', d=15),
-                Model(name='sphere', file_name='sphere_20', d=20),
-                Model(name='sphere', file_name='sphere_25', d=25)
+                Model(name='sphere', d=15),
+                Model(name='sphere', d=20),
+                Model(name='sphere', d=25)
             ]
         )
     ]
--- tests/examples/export_example_2.py
+++ tests/examples/export_example_2.py
@@ -6,15 +6,15 @@ exportables=Folder(
     contents=[
         Folder(
             name='cubes',
-            contents=[Model(name='cube', file_name='cube_{}'.format(size), x=size, y=size, z=size) for size in range(5, 16, 5)]
+            contents=[Model(name='cube', x=size, y=size, z=size) for size in range(5, 16, 5)]
         ),
         Folder(
             name='cylinders',
-            contents=[Model(name='cylinder', file_name='cylinder_{}'.format(height), d=10, z=height) for height in range(10, 31, 10)]
+            contents=[Model(name='cylinder', d=10, z=height) for height in range(10, 31, 10)]
         ),
         Folder(
             name='spheres',
-            contents=[Model(name='sphere', file_name='sphere_{}'.format(diameter), d=diameter) for diameter in range(15, 26, 5)]
+            contents=[Model(name='sphere', d=diameter) for diameter in range(15, 26, 5)]
         )
     ]
 )
